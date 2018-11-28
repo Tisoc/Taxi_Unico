af4a28ceefbef6af2df6d362a6ac68cd97460d90
Alvaro Marquez <lo_marquezcervantes@hotmail.com>
2018-11-28 12:09:11 -0600

    fix allDB.sql para que cree la bd si no existe

--- MySqlWorkBench_BD_Procedures/allDB.sql
+++ MySqlWorkBench_BD_Procedures/allDB.sql
@@ -3,7 +3,8 @@
 -- Host: 127.0.0.1    Database: taxiunico
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.1.31-MariaDB
-
+CREATE DATABASE IF NOT EXISTS taxiunico;
+USE taxiunico;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
